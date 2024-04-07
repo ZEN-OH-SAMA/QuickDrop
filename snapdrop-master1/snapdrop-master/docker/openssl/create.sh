a0b53ec885f36ea55e77d41ced8d672e242e259a
Swanand Bowalekar <53945157+ZEN-OH-SAMA@users.noreply.github.com>
2024-04-07 23:34:12 +0530

    Delete snapdrop-master1/snapdrop-master/docker directory

--- snapdrop-master1/snapdrop-master/docker/openssl/create.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cnf_dir='/mnt/openssl/'
-certs_dir='/etc/ssl/certs/'
-openssl req -config ${cnf_dir}quickdropCA.cnf -new -x509 -days 1 -keyout ${certs_dir}quickdropCA.key -out ${certs_dir}quickdropCA.crt
-openssl req -config ${cnf_dir}quickdropCert.cnf -new -out /tmp/quickdrop-dev.csr -keyout ${certs_dir}quickdrop-dev.key
-openssl x509 -req -in /tmp/quickdrop-dev.csr -CA ${certs_dir}quickdropCA.crt -CAkey ${certs_dir}quickdropCA.key -CAcreateserial -extensions req_ext -extfile ${cnf_dir}quickdropCert.cnf -sha512 -days 1 -out ${certs_dir}quickdrop-dev.crt
-
-exec "$@"
